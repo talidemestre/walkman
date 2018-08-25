f5f7e38be5d90b8b4d7cd82fb6e1cbc3d2128850
Tali <30426066+talidemestre@users.noreply.github.com>
2018-08-25 10:02:43 +1000

    a

---
+++ master.py
@@ -0,0 +1,38 @@
+import vlc
+import time
+
+#rewind start
+reStart=vlc.MediaPlayer("rewind_begin.wav")
+reMiddle = vlc.MediaPlayer("rewind_middle.wav")
+reEnd = vlc.MediaPlayer("rewind_end.wav")
+
+song = vlc.MediaPlayer("s2.mp3")
+
+def BeginRewind():
+  endSong=time.time()
+  song.pause()
+  reStart.play()
+  time.sleep(0.317)
+  reMiddle.play()
+  reStart.stop()
+  return (endSong)
+
+def EndRewind(beginSong, endSong):  
+  reMiddle.stop()
+  reEnd.play()
+  time.sleep(0.5)
+  song.play()
+  reEnd.stop()
+  song.set_time(int((endSong-beginSong)*1000))
+  return(time.time() - (endSong-beginSong))
+
+
+beginSong = time.time()
+while True:
+  song.play()
+
+  continued = input()
+  endSong  = BeginRewind()
+
+  continued = input()
+  beginSong = EndRewind(beginSong, endSong)
