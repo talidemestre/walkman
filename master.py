105ec3700c9e0d9c1c3844eaa96282141a789e46
Tali <30426066+talidemestre@users.noreply.github.com>
2018-08-26 13:04:00 +1000

    fastforward and rewind working

--- master.py
+++ master.py
@@ -14,12 +14,13 @@ reEnd = vlc.MediaPlayer("rewind_end.wav")
 
 song = vlc.MediaPlayer("s2.mp3")
 
-def BeginRewind():
+def BeginScrub():
   endSong=time.time()
   song.pause()
+  
   reStart.play()
-
   time.sleep(0.317)
+  
   reMiddle.play()
 
   reStart.stop()
@@ -33,15 +34,37 @@ def EndRewind(beginSong, endSong):
   newTime = time.time() - endSong
   song.set_time(int((endSong-beginSong)*1000) - int(newTime*3000))
   song.play()
-  return(time.time() - (endSong-beginSong) + (newTime))
+  return(time.time() - (endSong-beginSong) + (newTime*3))
 
+def EndForward(beginSong, endSong):
+  reMiddle.stop()
+  reEnd.play()
+  time.sleep(0.5)  
+  reEnd.stop()
+
+  newTime = time.time() - endSong
+  song.set_time(int((endSong-beginSong)*1000) + int(newTime*3000))
+  song.play()
+  return(time.time() - (endSong-beginSong) - (newTime*3))
+  
 
 beginSong = time.time()
 while True:
   song.play()
 
-  continued = input()
-  endSong  = BeginRewind()
+  continued = input('Major: ')
+  if continued =='r' or continued == 'f':
+    endSong  = BeginScrub()
+    continued = input('Minor: ')
 
-  continued = input()
-  beginSong = EndRewind(beginSong, endSong)
+    if continued == 'r':
+      beginSong = EndRewind(beginSong, endSong)
+
+    elif continued == 'f':
+      beginSong = EndForward(beginSong, endSong)
+
+#note to solve the lag time between pressing buttons the middle audio file could be rendered with 0.317 second gap at start
+    
+
+
+#must handl
